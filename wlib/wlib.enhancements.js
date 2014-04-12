20f4107e9bfe56edeffbe76e6bf49bfbe274c319
Nicolas Bages <nicolas.bages@weinto.com>
2014-04-12 06:25:21 +0200

    Add polyfil for array.some()

--- wlib/wlib.enhancements.js
+++ wlib/wlib.enhancements.js
@@ -20,6 +20,31 @@ Array.prototype.remove = function(from, to)
 	return this.push.apply(this, rest);
 };
 
+if (!Array.prototype.some)
+{
+  Array.prototype.some = function(fun /*, thisArg */)
+  {
+    'use strict';
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function')
+      throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t && fun.call(thisArg, t[i], i, t))
+        return true;
+    }
+
+    return false;
+  };
+}
+
 if(typeof jQuery == 'function' && typeof jQuery.fn.jquery == 'string')
 {
 	$(window).on('resize', function()
